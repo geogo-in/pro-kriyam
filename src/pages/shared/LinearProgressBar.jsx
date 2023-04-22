1accb2b71b71aca1c6495d178a18cdac8e0faab0
Avishek Jana <avishek@geogo.in>
2023-04-22 20:25:01 +0530

    Progressbar color updayed

--- src/pages/shared/LinearProgressBar.jsx
+++ src/pages/shared/LinearProgressBar.jsx
@@ -2,7 +2,7 @@ import { LinearProgress } from "@mui/material"
 import React from "react"
 
 export default function LinearProgressBar() {
-  return <LinearProgress color="secondary" style={styles} />
+  return <LinearProgress color="primary" style={styles} />
 }
 
 const styles = {
@@ -10,7 +10,7 @@ const styles = {
   top: 0,
   left: 0,
   width: "100%",
-  height: 2,
+  height: 3,
   zIndex: 9999,
   backgroundColor: "rgb(255, 255, 255, 0.4)",
 }
