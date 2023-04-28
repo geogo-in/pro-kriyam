6f14d617fe2c297d5945bc475281008efa8cfc7a
Dipanjan <dipanjan.panja@geogo.in>
2023-04-28 18:32:51 +0530

    update: FCM config

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+import firebaseConfig from "config/firebase"
 import { getAnalytics } from "firebase/analytics"
 import { initializeApp } from "firebase/app"
 import { getMessaging, getToken, onMessage } from "firebase/messaging"
@@ -8,15 +9,6 @@ import { getMessaging, getToken, onMessage } from "firebase/messaging"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_API_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-}
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig)
