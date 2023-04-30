4ea6f94d7e4ba15c28b7e495596f45ba3f0c4914
Dipanjan <dipanjan.panja@geogo.in>
2023-04-30 11:27:21 +0530

    update: sw

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import react from "@vitejs/plugin-react-swc"
 import * as path from "path"
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   resolve: {
     alias: [
       { find: "assets", replacement: path.resolve(__dirname, "src/assets") },
