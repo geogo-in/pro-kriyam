6372177119149cdf6a39f98783fb267e1dd75ada
Dipanjan <dipanjan.panja@geogo.in>
2023-05-01 13:36:27 +0530

    update: lock user

--- src/pages/members/Components/MembersTable.jsx
+++ src/pages/members/Components/MembersTable.jsx
@@ -1,16 +1,20 @@
-import { Link, Typography } from "@mui/material"
+import { Lock, LockOpen } from "@mui/icons-material"
+import { CircularProgress, IconButton, Link, Tooltip, Typography } from "@mui/material"
 import Table from "@mui/material/Table"
 import TableBody from "@mui/material/TableBody"
 import TableContainer from "@mui/material/TableContainer"
 import TableHead from "@mui/material/TableHead"
 import TableRow from "@mui/material/TableRow"
+import { useUpdateUserMutation } from "@redux/services/userApi"
+import { enqueueSnackbar } from "notistack"
 import MemberAvatar from "pages/shared/MemberAvatar"
 import { StyledTableRow, TableCell, TableHeadCell } from "pages/shared/StyledTable"
 import { Link as RouterLink } from "react-router-dom"
 import { PATH_DASHBOARD } from "routes/paths"
 import { fDate } from "utils/formatDate"
+import { getErrorMessage } from "utils/helper"
 
-const MembersTable = ({ users }) => (
+const MembersTable = ({ users, lockedUsers, registeredUsers }) => (
   <TableContainer sx={{ borderRadius: 0 }}>
     <Table aria-label="simple table">
       <TableHead sx={{ backgroundColor: "white", borderRadius: 0, borderBottom: "none" }}>
@@ -21,11 +25,18 @@ const MembersTable = ({ users }) => (
           <TableHeadCell>Email</TableHeadCell>
           <TableHeadCell>Role</TableHeadCell>
           <TableHeadCell>User since</TableHeadCell>
+          <TableHeadCell align="center">Lock user</TableHeadCell>
         </TableRow>
       </TableHead>
       <TableBody>
         {users.map(row => (
           <ProjectItem key={row.id} {...row} />
+        ))}
+        {registeredUsers.map(row => (
+          <ProjectItem key={row.id} {...row} registered />
+        ))}
+        {lockedUsers.map(row => (
+          <ProjectItem key={row.id} {...row} locked />
         ))}
       </TableBody>
     </Table>
@@ -34,7 +45,17 @@ const MembersTable = ({ users }) => (
 
 export default MembersTable
 
-function ProjectItem(row) {
+function ProjectItem({ locked, registered, ...row }) {
+  const [updateUser, { isLoading }] = useUpdateUserMutation()
+  const handleUpdateUser = async () => {
+    try {
+      await updateUser({ id: row.id, status: locked ? 1 : 3 }).unwrap()
+      enqueueSnackbar("User updated successfully", { variant: "success" })
+    } catch (error) {
+      const { message } = getErrorMessage(error)
+      enqueueSnackbar(message, { variant: "error" })
+    }
+  }
   return (
     <StyledTableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
       <TableCell>
@@ -62,6 +83,23 @@ function ProjectItem(row) {
         {row.admin ? <Typography sx={{ fontWeight: 500 }}>Admin</Typography> : "Normal user"}
       </TableCell>
       <TableCell>{fDate(row.created_on)}</TableCell>
+      <TableCell align="center">
+        <IconButton onClick={handleUpdateUser} disabled={isLoading}>
+          {isLoading ? (
+            <CircularProgress size={20} />
+          ) : registered ? (
+            <></>
+          ) : locked ? (
+            <Tooltip title="Un-lock user">
+              <LockOpen />
+            </Tooltip>
+          ) : (
+            <Tooltip title="Lock user">
+              <Lock />
+            </Tooltip>
+          )}
+        </IconButton>
+      </TableCell>
     </StyledTableRow>
   )
 }
