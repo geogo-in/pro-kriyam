142db5dfb25e06e84f6d13c7992212142c0f77c8
Dipanjan <dipanjan.panja@geogo.in>
2023-05-11 11:54:57 +0530

    update: chunk

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import react from "@vitejs/plugin-react-swc"
 import * as path from "path"
-import { defineConfig } from "vite"
+import { defineConfig, splitVendorChunkPlugin } from "vite"
 import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    splitVendorChunkPlugin(),
     VitePWA({
       registerType: "autoUpdate",
       workbox: { maximumFileSizeToCacheInBytes: 3_0_00_000 },
